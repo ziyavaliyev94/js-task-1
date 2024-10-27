47a84ac35a67c6d74e9e196c89be88003d42dbd3
ziyavaliyev94 <ziya.valiyev.1994@gmail.com>
2024-10-28 01:38:10 +0400

    home-task-1

---
+++ h-task-1/main.js
@@ -0,0 +1,147 @@
+// sual-1
+
+// let number = prompt("Reqem Yaz!")
+
+// if (number > 0) {
+//     console.log("reqem musbetdir!");
+
+// }
+// else if (number < 0) {
+//     console.log("reqem menfidir!");
+
+// }
+// else {
+//     console.log("reqem 0-a beraberdir");
+
+// }
+
+// sual-2
+
+// let user = "admin"; 
+
+// switch (user) {
+//     case "admin":
+//         console.log("Admin girishi aktiv edildi");
+//         break;
+//     case "istifadəçi":
+//         console.log("stifadeci girishi aktiv edildi");
+//         break;
+//     case "qonaq":
+//         console.log("Qonaq girishi aktiv edildi");
+//         break;
+//     default:
+//         console.log("Rol duzgun deyil");
+// }
+
+
+// sual-3
+
+// if (number % 2 == 0) {
+//     console.log(number,  "reqemi cutdur");
+    
+// }
+// else{
+//     console.log(number,  "reqemi tekdir");
+    
+// }
+
+// sual-5
+
+// var number = 5
+// let number2 = "5"
+// console.log(number == number2);
+// console.log(number === number2);
+
+// sual-6
+
+// let number = 10
+// console.log(number !== 10);
+
+// sual-7
+
+// if(number > 5 && number < 10){
+//     console.log(number, "5-den boyuk 10-dan kicikdir");
+    
+// }
+// else{
+//     console.log(number, "5-den boyuk 10-dan kicik deyil");
+    
+// }
+
+// sual-8
+
+// let name1 = null;
+
+// let name2 = name1 ?? "Anonim";
+
+// console.log(name2);
+
+// sual-9
+
+// let number = 10
+// console.log(number);
+// console.log(!number);
+
+
+// sula-10
+
+// let number1 = 1
+// let number2 = 2
+
+// if(number1 && number2){
+//    console.log("true");
+// }
+// else {
+//    console.log("false");
+// }
+
+// sual-11
+
+// let number1 = 7 < 5
+// let number2 = 5 < 7
+
+// if(number1 || number2){
+//    console.log("biri sehv digeri duzdur");   
+// }
+// else{
+//    console.log("ikisi de sehvdir");
+// }
+
+// sual-12
+
+// let number = 6;
+
+// if (number > 0 && number % 2 == 0) {
+//     console.log("hem musbet hem de cutdur");
+// } else {
+//     console.log("musbet ve ya cut deyil");
+// }
+
+// sual-13
+
+// let username = "user123";
+// let password = "password123";
+
+
+// let trueUsername = "user123";
+// let truePassword = "password123";
+
+
+// if (username === trueUsername && password === truePassword) {
+//     console.log("Etibarlidir");
+// } else {
+//     console.log("Etibarsizdir");
+// }
+
+// sual-14
+
+// let number = -2;
+
+// switch (true) {
+//     case (number < 0):
+//         console.log("eded menfidir");
+//         break;
+//     case (number == 0):
+//         console.log("eded sifirdir");
+// }
+
